e674d8ee028bd150eb7f1134966fde203e0d91b4
Ravi Garbuja Pun <cbpmgr@gmail.com>
2020-07-23 21:31:49 +0545

    feat: Update sample app to show bold label text usage

--- app/src/main/java/com/noowenz/showmoreless/MainActivity.kt
+++ app/src/main/java/com/noowenz/showmoreless/MainActivity.kt
@@ -16,35 +16,36 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         ShowMoreLess.Builder(this)
-                /*.textLengthAndLengthType(
-                        length = 100,
-                        textLengthType = ShowMoreLess.TYPE_CHARACTER
-                )*/
-                .textLengthAndLengthType(
-                        length = 5,
-                        textLengthType = ShowMoreLess.TYPE_LINE
-                )
-                .showMoreLabel("show more")
-                .showLessLabel("show less")
-                .showMoreLabelColor(Color.parseColor("#FF0000"))
-                .showLessLabelColor(Color.parseColor("#FF0000"))
-                .labelUnderLine(labelUnderLine = true)
-                .expandAnimation(expandAnimation = true)
-                .textClickable(
-                        textClickableInExpand = true,
-                        textClickableInCollapse = true
-                )
-                .build().apply {
-                    addShowMoreLess(textView = tv_first, text = tv_first.text, isContentExpanded = false)
-                    setListener(object : ShowMoreLess.OnShowMoreLessClickedListener {
-                        override fun onShowMoreClicked() {
-                            //We can handle or save show more state
-                        }
+            /*.textLengthAndLengthType(
+                    length = 100,
+                    textLengthType = ShowMoreLess.TYPE_CHARACTER
+            )*/
+            .textLengthAndLengthType(
+                length = 5,
+                textLengthType = ShowMoreLess.TYPE_LINE
+            )
+            .showMoreLabel("show more")
+            .showLessLabel("show less")
+            .showMoreLabelColor(Color.parseColor("#FF0000"))
+            .showLessLabelColor(Color.parseColor("#FF0000"))
+            .labelUnderLine(labelUnderLine = true)
+            .labelBold(labelBold = true)
+            .expandAnimation(expandAnimation = true)
+            .textClickable(
+                textClickableInExpand = true,
+                textClickableInCollapse = true
+            )
+            .build().apply {
+                addShowMoreLess(textView = tv_first, text = tv_first.text, isContentExpanded = false)
+                setListener(object : ShowMoreLess.OnShowMoreLessClickedListener {
+                    override fun onShowMoreClicked() {
+                        //We can handle or save show more state
+                    }
 
-                        override fun onShowLessClicked() {
-                            //We can handle or save show less state
-                        }
-                    })
-                }
+                    override fun onShowLessClicked() {
+                        //We can handle or save show less state
+                    }
+                })
+            }
     }
 }
